89ec2590fd565362983988a108999b57f728a7ff
Lucas Soares <icloud.lucasc@gmail.com>
2024-12-22 23:23:43 -0300

    chore: configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,27 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/containers/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				blue: {
+					500: "#2188FF",
+					700: "#17192D",
+					900: "#023B78",
+				},
+				gray: {
+					150: "#E3EAEF",
+				},
+				background: "var(--background)",
+				foreground: "var(--foreground)",
+			},
+		},
+	},
+	plugins: [],
 } satisfies Config;
